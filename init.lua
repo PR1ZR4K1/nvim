fe75e055a450dbbf65f4421cb7550fe1eabd6e41
jignacio <jaylonignacio@gmail.com>
2026-07-18 17:30:41 -0700

    feat: honor startup directories and disable browser hijacking

--- init.lua
+++ init.lua
@@ -4,6 +4,21 @@ if vim.fn.has("nvim-0.9.4") == 0 then
   return
 end
 
+-- Disable netrw so opening directories does not spawn the built-in browser.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- If Neovim is launched with a single directory argument, make that directory
+-- the actual working directory so cwd-based plugins (Telescope, Neo-tree,
+-- sessions, etc.) all agree on the root context.
+if vim.fn.argc() == 1 then
+  local launch_arg = vim.fn.argv(0)
+  if vim.fn.isdirectory(launch_arg) == 1 then
+    vim.g.startup_dir_arg = vim.fn.fnamemodify(launch_arg, ":p")
+    vim.cmd.cd(vim.fn.fnameescape(vim.g.startup_dir_arg))
+  end
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -27,3 +42,31 @@ require("nvim-keymaps")
 
 require("lazy").setup("plugins")
 
+if vim.g.startup_dir_arg then
+  local startup_group = vim.api.nvim_create_augroup("UserStartupDirectory", { clear = true })
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = startup_group,
+    once = true,
+    callback = function()
+      if vim.v.this_session ~= "" then
+        return
+      end
+
+      local current_buf = vim.api.nvim_get_current_buf()
+      local current_name = vim.api.nvim_buf_get_name(current_buf)
+
+      if current_name == "" or vim.fn.isdirectory(current_name) ~= 1 then
+        return
+      end
+
+      local opened_alpha = pcall(vim.cmd, "Alpha")
+      if not opened_alpha then
+        vim.cmd.enew()
+      end
+
+      if vim.api.nvim_get_current_buf() ~= current_buf then
+        pcall(vim.api.nvim_buf_delete, current_buf, { force = true })
+      end
+    end,
+  })
+end
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     require("neo-tree").setup({
       filesystem = {
+        hijack_netrw_behavior = "disabled",
         filtered_items = {
           visible = true,        -- show hidden files (dimmed)
           hide_dotfiles = false, -- don't hide dotfiles
